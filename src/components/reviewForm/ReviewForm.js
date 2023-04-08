c50e734d07f1bc5d274e6b10956c39a786499a2f
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-04-09 01:35:15 +0530

    final commit

---
+++ src/components/reviewForm/ReviewForm.js
@@ -0,0 +1,17 @@
+import { Form, Button } from "react-bootstrap"
+
+
+
+const ReviewForm = ({ handleSubmit, revText, labelText, defaultValue }) => {
+    return (
+        <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextArea1">
+                <Form.Label>{labelText} </Form.Label>
+                <Form.Control ref={revText} as='textarea' defaultValue={defaultValue} />
+            </Form.Group>
+            <Button variant="outline-info" onClick={handleSubmit}>Submit</Button>
+        </Form>
+    )
+}
+
+export default ReviewForm
--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+// required packages
+// axios,react-bootstrap,bootstrap,@fortawesome/react-fontawesome,@fortawesome/free-solid-svg-icons,react-player,react-router-dom,@mui/material @emotion/react @emotion/styled,react-material-ui-carousel
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/*' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
